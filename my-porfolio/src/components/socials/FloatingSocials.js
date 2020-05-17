05dd2026b5baad8af10c9ade665ada5219b8f64d
Ishwant Singh <ishwant8@gmail.com>
2020-05-18 00:10:56 +0530

    css: floating social container.

--- my-porfolio/src/components/socials/FloatingSocials.js
+++ my-porfolio/src/components/socials/FloatingSocials.js
@@ -7,11 +7,32 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
+  align-self: flex-end;
+  margin-bottom: 5em;
+`;
+const SubContainer = styled.div`
+  width: 70%;
+  height: 20%;
+  flex-direction: column;
+  wrap-content: wrap;
+  justify-content: center;
   align-items: center;
 `;
-
+const FloatingLink = styled.div`
+  width: 50%;
+  margin: 0 auto;
+`;
 const FloatingSocials = () => {
-  return <Container>FloatingSocials</Container>;
+  return (
+    <Container>
+      <SubContainer>
+        <FloatingLink>Linkedin</FloatingLink>
+        <FloatingLink>Twitter</FloatingLink>
+        <FloatingLink>G-mail</FloatingLink>
+        <FloatingLink>Github</FloatingLink>
+      </SubContainer>
+    </Container>
+  );
 };
 
 export default FloatingSocials;
