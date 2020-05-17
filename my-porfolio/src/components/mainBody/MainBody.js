e8e3ef13aa781991acd12b5a93e4e63b624d02b2
Ishwant Singh <ishwant8@gmail.com>
2020-05-17 23:04:59 +0530

    update: MainBody.js displayed

---
+++ my-porfolio/src/components/mainBody/MainBody.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+// import Link from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justifycontent: center;
+  alignitems: center;
+`;
+
+const MainBody = () => {
+  return <Container>hello</Container>;
+};
+
+export default MainBody;
