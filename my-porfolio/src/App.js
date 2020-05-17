67f337442a9b60647df193bd777ace587af7a40a
Ishwant Singh <ishwant8@gmail.com>
2020-05-17 23:46:09 +0530

    css: sizes set of basic pages.

--- my-porfolio/src/App.js
+++ my-porfolio/src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 
+import FloatingSocials from "./components/socials/FloatingSocials";
 import MainBody from "./components/mainBody/MainBody";
+import NavContainer from "./components/navigation/NavContainer";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact path="/">
-          <MainBody />
-        </Route>
+        <Container>
+          <FloatingSocials />
+          <Switch>
+            <Route exact path="/">
+              <MainBody />
+            </Route>{" "}
+          </Switch>
+          <NavContainer />
+        </Container>
       </Router>
     );
   }
---
+++ my-porfolio/src/components/socials/FloatingSocials.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+// import Link from "react-router-dom";
+
+const Container = styled.div`
+  width: 10%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FloatingSocials = () => {
+  return <Container>FloatingSocials</Container>;
+};
+
+export default FloatingSocials;
