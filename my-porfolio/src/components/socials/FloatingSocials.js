2d2a5419df50adc766e57604b15dfa2a4136ffaa
Ishwant Singh <ishwant8@gmail.com>
2020-05-18 03:02:37 +0530

    Add: floating icons added. working.

--- my-porfolio/src/components/socials/FloatingSocials.js
+++ my-porfolio/src/components/socials/FloatingSocials.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Icon, Menu } from "semantic-ui-react";
 
 const Container = styled.div`
   width: 10%;
@@ -7,28 +8,69 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-self: flex-end;
-  margin-bottom: 5em;
+  margin-bottom: 2em;
 `;
 const SubContainer = styled.div`
-  width: 70%;
-  height: 20%;
+  width: 50%;
+  height: 30%;
   flex-direction: column;
   wrap-content: wrap;
   justify-content: center;
-  align-items: center;
+  align-items: space-between;
+  .icons {
+    margin: 0.5rem auto;
+  }
 `;
-const FloatingLink = styled.div`
-  width: 50%;
-  margin: 0 auto;
-`;
+// const FloatingLink = styled.div`
+//   width: 50%;
+//   margin: 0 auto;
+// `;
 const FloatingSocials = () => {
   return (
     <Container>
       <SubContainer>
-        <FloatingLink>Linkedin</FloatingLink>
-        <FloatingLink>Twitter</FloatingLink>
-        <FloatingLink>G-mail</FloatingLink>
-        <FloatingLink>Github</FloatingLink>
+        <Menu.Item
+          href="https://www.linkedin.com/in/ishwant-singh/"
+          position="left"
+          target="_blank"
+        >
+          <Icon
+            link
+            name="linkedin"
+            color="black"
+            size="huge"
+            className="icons"
+          />
+        </Menu.Item>
+        <Menu.Item
+          href="https://twitter.com/ishwant_singh"
+          position="left"
+          target="_blank"
+        >
+          <Icon
+            link
+            name="twitter"
+            color="black"
+            size="huge"
+            className="icons"
+          />
+        </Menu.Item>
+        <Menu.Item href="ishwant2001@gmail.com" position="left" target="_blank">
+          <Icon link name="mail" color="black" size="huge" className="icons" />
+        </Menu.Item>
+        <Menu.Item
+          href="https://github.com/ishwantsingh"
+          position="left"
+          target="_blank"
+        >
+          <Icon
+            link
+            name="github"
+            color="black"
+            size="huge"
+            className="icons"
+          />
+        </Menu.Item>
       </SubContainer>
     </Container>
   );
