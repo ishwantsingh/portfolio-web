2dea60a3c5bba1be9819f5dec5f1ce13820f2270
Ishwant Singh <ishwant8@gmail.com>
2020-05-18 00:40:14 +0530

    css: nav container.

--- my-porfolio/src/components/navigation/NavContainer.js
+++ my-porfolio/src/components/navigation/NavContainer.js
@@ -6,12 +6,37 @@ const Container = styled.div`
   width: 25%;
   height: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
 `;
-
+const SubContainer = styled.div`
+  width: 80%;
+  height: 50%;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const FloatingLink = styled.div`
+  width: 100%;
+  height: 2.2rem;
+  font-size: 1.1rem;
+  margin: 1.75rem 0;
+  justify-self: center;
+  //   align-self: center;
+  border-left: 2px solid black;
+  text-align: center;
+`;
 const NavContainer = () => {
-  return <Container>NavContainer</Container>;
+  return (
+    <Container>
+      <SubContainer>
+        <FloatingLink>About</FloatingLink>
+        <FloatingLink>Projects</FloatingLink>
+        <FloatingLink>Skills</FloatingLink>
+        <FloatingLink>Socials</FloatingLink>
+      </SubContainer>
+    </Container>
+  );
 };
 
 export default NavContainer;
