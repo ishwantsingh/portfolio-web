dd727ca73f0391785fcd23ecda02b10f054f4b03
Ishwant Singh <ishwant8@gmail.com>
2020-05-19 14:10:59 +0530

    add: bgimage and assets added.

--- my-porfolio/src/components/mainBody/About.js
+++ my-porfolio/src/components/mainBody/About.js
@@ -1,31 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import bgImage from "../../assets/bgImage.svg";
+import designer from "../../assets/devProd.svg";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  border-right: 1.5px solid #e1e0e0;
-  border-left: 1.5px solid #e1e0e0;
-  background-color: #f9f9f9;
+  border-right: 1px solid #e1e0e0;
+  border-left: 1px solid #e1e0e0;
+  background-color: #ffffff;
   font-size: 7rem;
   background-image: url(${bgImage});
   background-repeat: no-repeat;
   background-size: cover;
+  .imageDiv {
+    width: 35%;
+    height: 40vh;
+    display: flex;
+    justify-content: flex-start;
+    align-self: flex-start;
+    margin: 3rem 0 0 3rem;
+  }
 `;
-// const DevImg = styled.image`
-//   width: 100%;
-//   height: 100%;
-// `;
+
+// DAF8FF f9f9f9
+const DevImg = styled.img`
+  width: 25vw;
+  display: flex;
+  justify-self: flex-start;
+  align-self: flex-start;
+`;
 
 const About = () => {
   return (
     <Container>
-      {/* <DevImg src={bgImage} alt="background image" /> */}
-      About
+      <div className="imageDiv">
+        <DevImg src={designer} alt="designer image" />
+      </div>
+
+      {/* <div>About</div> */}
     </Container>
   );
 };
