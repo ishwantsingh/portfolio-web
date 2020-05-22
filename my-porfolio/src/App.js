bf2f2fc2a1df01e74acadbb4ae515a08ffc763a8
Ishwant Singh <ishwant8@gmail.com>
2020-05-22 19:34:12 +0530

    feat: changing page by scrolling reverses animation too.

--- my-porfolio/src/App.js
+++ my-porfolio/src/App.js
@@ -2,15 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import history from "./history";
-// import { Transition, TransitionGroup } from "react-transition-group";
 
-import {
-  AboutTimeline,
-  ProjectsTimeline,
-  SkillsTimeline,
-  HobbiesTimeline,
-} from "./components/animations/Timelines";
-// import { play, exit } from "./components/animations/Timelines";
 import About from "./components/mainBody/About";
 import Projects from "./components/mainBody/Projects";
 import Skills from "./components/mainBody/Skills";
@@ -35,89 +27,33 @@ class App extends React.Component {
   componentDidMount() {
     window.addEventListener("wheel", this.throttle(this.scrollChangePage, 900));
   }
-  getAboutPageChange = function (e, destination) {
-    this.aboutChild.current.changePage(e, destination);
-  };
-  getProjectsChange = function (e, destination) {
-    this.projectsChild.current.changePage(e, destination);
-  };
-  getSkillsChange = function (e, destination) {
-    this.skillsChild.current.changePage(e, destination);
-  };
-  getHobbiesChange = function (e, destination) {
-    this.hobbiesChild.current.changePage(e, destination);
-  };
-  // AboutCompletePromise = new Promise(function (resolve, reject) {
-  //   this.getPageChangeData();
-  //   resolve("animation done");
-  //   reject("oops!");
-  // });
-  // ProjectsCompletePromise = new Promise(function (resolve) {
-  //   ProjectsTimeline();
-  //   resolve("animation done");
-  // });
-  // SkillsCompletePromise = new Promise(function (resolve) {
-  //   SkillsTimeline();
-  //   resolve("animation done");
-  // });
-  // HobbiesCompletePromise = new Promise(function (resolve) {
-  //   HobbiesTimeline();
-  //   resolve("animation done");
-  // });
 
-  scrollChangePage = function (e) {
+  scrollChangePage = (e) => {
     const isScrollingDown = Math.sign(e.deltaY);
     if (isScrollingDown === -1 && history.location.pathname === "/projects") {
-      // this.AboutCompletePromise.then(() =>
-      // history.replace("/");
-      // );
-      document.getElementById("to-about-but").click();
-      // ChangePage(e, "/");
+      this.projectsChild.current.changePage(e, "/");
     } else if (isScrollingDown === 1 && history.location.pathname === "/") {
-      // this.ProjectsCompletePromise.then(
-      document.getElementById("to-projects-but").click();
-      //  return this.getPageChangeData(e, "/projects");
-
-      //  history.replace("/projects");
-      // );
-
-      // ChangePage(e, "/projects");
+      this.aboutChild.current.changePage(e, "/projects");
     } else if (
       isScrollingDown === 1 &&
       history.location.pathname === "/projects"
     ) {
-      // this.SkillsCompletePromise.then(
-      // history.replace("/skills");
-      // );
-      // ChangePage(e, "/skills");
-      document.getElementById("to-skills-but").click();
+      this.projectsChild.current.changePage(e, "/skills");
     } else if (
       isScrollingDown === 1 &&
       history.location.pathname === "/skills"
     ) {
-      // this.HobbiesCompletePromise.then(
-      // history.replace("/hobbies");
-      // );
-      document.getElementById("to-hobbies-but").click();
-      // ChangePage(e, "/hobbies");
+      this.skillsChild.current.changePage(e, "/hobbies");
     } else if (
       isScrollingDown === -1 &&
       history.location.pathname === "/skills"
     ) {
-      // this.ProjectsCompletePromise.then(
-      // history.replace("/projects");
-      // );
-      document.getElementById("to-projects-but").click();
-      // ChangePage(e, "/projects");
+      this.skillsChild.current.changePage(e, "/projects");
     } else if (
       isScrollingDown === -1 &&
       history.location.pathname === "/hobbies"
     ) {
-      // this.SkillsCompletePromise.then(
-      // history.replace("/skills");
-      // );
-      document.getElementById("to-skills-but").click();
-      // ChangePage(e, "/skills");
+      this.hobbiesChild.current.changePage(e, "/skills");
     }
   };
 
@@ -138,52 +74,18 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-        <button
-          id="to-about-but"
-          onClick={(e) => this.getProjectsChange(e, "/")}
-          style={{ display: "none" }}
-        >
-          Click
-        </button>{" "}
-        <button
-          id="to-projects-but"
-          onClick={(e) => this.getAboutPageChange(e, "/projects")}
-          style={{ display: "none" }}
-        >
-          Click
-        </button>
-        <button
-          id="to-skills-but"
-          onClick={(e) => this.getAboutPageChange(e, "/skills")}
-          style={{ display: "none" }}
-        >
-          Click
-        </button>{" "}
-        <button
-          id="to-hobbies-but"
-          onClick={(e) => this.getAboutPageChange(e, "/hobbies")}
-          style={{ display: "none" }}
-        >
-          Click
-        </button>
         <Switch>
           <Route exact path="/">
-            <About AboutTimeline={AboutTimeline} ref={this.aboutChild} />
+            <About ref={this.aboutChild} />
           </Route>
           <Route path="/projects">
-            <Projects
-              ProjectsTimeline={ProjectsTimeline}
-              ref={this.projectsChild}
-            />
+            <Projects ref={this.projectsChild} />
           </Route>
           <Route path="/skills">
-            <Skills SkillsTimeline={SkillsTimeline} ref={this.skillsChild} />
+            <Skills ref={this.skillsChild} />
           </Route>
           <Route path="/hobbies">
-            <Hobbies
-              HobbiesTimeline={HobbiesTimeline}
-              ref={this.hobbiesChild}
-            />
+            <Hobbies ref={this.hobbiesChild} />
           </Route>
         </Switch>
         <NavContainer />
--- my-porfolio/src/components/animations/Timelines.js
+++ my-porfolio/src/components/animations/Timelines.js
@@ -1,44 +1,44 @@
-import { TimelineMax, Power1 } from "gsap";
+// import { TimelineMax, Power1 } from "gsap";
 
-export const AboutTimeline = (elementRef) => {
-  const Timeline1 = new TimelineMax();
-  Timeline1.from(elementRef, 1, {
-    autoAlpha: 0,
-    delay: 0,
-    ease: Power1.easeIn,
-  })
-    .staggerFrom(
-      "#texts",
-      0.475,
-      { autoAlpha: 0, x: -25, ease: Power1.easeOut, delay: 0.5 },
-      0.155
-    )
-    .reverse();
-};
-export const ProjectsTimeline = () => {
-  const Timeline2 = new TimelineMax();
-  Timeline2.from("#content-2", 1.5, {
-    autoAlpha: 0,
-    delay: 0,
-    ease: Power1.easeIn,
-  }).reverse();
-};
-export const SkillsTimeline = () => {
-  const Timeline3 = new TimelineMax();
-  Timeline3.from("#content-3", 1.5, {
-    autoAlpha: 0,
-    delay: 0,
-    ease: Power1.easeIn,
-  }).reverse();
-};
-export const HobbiesTimeline = () => {
-  const Timeline4 = new TimelineMax();
-  Timeline4.from("#content-4", 1.5, {
-    autoAlpha: 0,
-    delay: 0,
-    ease: Power1.easeIn,
-  }).reverse();
-};
+// export const AboutTimeline = (elementRef) => {
+//   const Timeline1 = new TimelineMax();
+//   Timeline1.from(elementRef, 1, {
+//     autoAlpha: 0,
+//     delay: 0,
+//     ease: Power1.easeIn,
+//   })
+//     .staggerFrom(
+//       "#texts",
+//       0.475,
+//       { autoAlpha: 0, x: -25, ease: Power1.easeOut, delay: 0.5 },
+//       0.155
+//     )
+//     .reverse();
+// };
+// export const ProjectsTimeline = () => {
+//   const Timeline2 = new TimelineMax();
+//   Timeline2.from("#content-2", 1.5, {
+//     autoAlpha: 0,
+//     delay: 0,
+//     ease: Power1.easeIn,
+//   }).reverse();
+// };
+// export const SkillsTimeline = () => {
+//   const Timeline3 = new TimelineMax();
+//   Timeline3.from("#content-3", 1.5, {
+//     autoAlpha: 0,
+//     delay: 0,
+//     ease: Power1.easeIn,
+//   }).reverse();
+// };
+// export const HobbiesTimeline = () => {
+//   const Timeline4 = new TimelineMax();
+//   Timeline4.from("#content-4", 1.5, {
+//     autoAlpha: 0,
+//     delay: 0,
+//     ease: Power1.easeIn,
+//   }).reverse();
+// };
 
 // export const ChangePage = (e, destination) => {
 //   e.preventDefault();
