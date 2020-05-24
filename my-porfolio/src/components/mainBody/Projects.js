4040417cf0e86e20d1937c99fad4abc85490adf7
Ishwant Singh <ishwant8@gmail.com>
2020-05-25 04:51:00 +0530

    add: icons and styling.

--- my-porfolio/src/components/mainBody/Projects.js
+++ my-porfolio/src/components/mainBody/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { TimelineMax, Power1 } from "gsap/all";
+import { Icon } from "semantic-ui-react";
 import history from "../../history";
 
 import postit from "../../assets/post-it.mp4";
@@ -12,9 +13,15 @@ const Container = styled.div`
   flex-direction:column;
   justify-content: center;
   align-items: center;
-  // border-right: 1.5px solid #e1e0e0;
   background-color: white;
   font-size: 4rem;
+  .project-content {
+    width: 100%;
+    display: flex;
+    flex-direction:row;
+    justify-content: space-evenly;
+    align-items: center;
+  }
   .projectVidContainer{
     margin-top: 5rem;
     width: 67vw;
@@ -22,15 +29,24 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    border 1px dashed #333;
-    border-radius: 5px;
+    border 1px dashed rgb(224,222,222);
+    border-radius: 6px;
   }
   .projectVid{
-    width: 60vw;
-    // height: 65vh;
-    // border 1px solid #333;
-    box-shadow: 0px 24px 48px #6b6b6b;
-    border-radius: 8px;
+    width: 61vw;
+    box-shadow: 0px 18px 36px #6b6b6b;
+    border-radius: 10px;
+  }
+  .icon {
+    width: 5%;
+    text-shadow: 0px 8px 20px #D0D0D0;
+    cursor: pointer;
+    background-color: white;
+    color: #303030
+  }
+  i.icon:hover {
+    font-size: 4.2rem;
+    text-shadow: 0px 0px 0px #FFFFFF;
   }
 `;
 
@@ -41,11 +57,18 @@ class Projects extends React.Component {
   }
 
   componentDidMount() {
-    this.timeline.from("#content-2", 1.5, {
-      autoAlpha: 0,
-      delay: 0,
-      ease: Power1.easeIn,
-    });
+    this.timeline
+      .from("#content-2", 1.5, {
+        autoAlpha: 0,
+        delay: 0,
+        ease: Power1.easeIn,
+      })
+      .staggerFrom(
+        "#video",
+        0.475,
+        { autoAlpha: 0, x: -25, ease: Power1.easeOut, delay: 0.1 },
+        0.185
+      );
     this.timeline.play();
   }
 
@@ -62,22 +85,27 @@ class Projects extends React.Component {
     return (
       <Container id="content-2">
         <div>Post It!</div>
-        <div class="projectVidContainer">
-          <a href="https://post-it-8.firebaseapp.com/" target="_blank">
-            <video
-              class="projectVid"
-              playsInline="true"
-              src={postit}
-              type="video/mp4"
-              muted
-              autoPlay
-              loop
-              preload="auto"
-            >
-              {" "}
-              Your browser doesn't support HTML5 video tag.
-            </video>
-          </a>
+        <div className="project-content">
+          <Icon name="chevron circle left" className="icon" />
+          <div class="projectVidContainer" id="video">
+            <a href="https://post-it-8.firebaseapp.com/" target="_blank">
+              <video
+                id="video"
+                class="projectVid"
+                playsInline="true"
+                src={postit}
+                type="video/mp4"
+                muted
+                autoPlay
+                loop
+                preload="auto"
+              >
+                {" "}
+                Your browser doesn't support HTML5 video tag.
+              </video>
+            </a>
+          </div>
+          <Icon name="chevron circle right" className="icon" />
         </div>
       </Container>
     );
